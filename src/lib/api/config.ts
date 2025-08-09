bfccb4fee8b3d8b2749ac0544ca485b8453efb12
João Paulo <ramoslopesjoaopaulo@gmail.com>
2025-08-09 00:02:05 -0300

    update

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -3,7 +3,7 @@ export const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://127.0.0.1:1
 // Log para verificar se a variável de ambiente está sendo lida no build da Vercel
 const tokenForLog = process.env.STRAPI_BACKEND_TOKEN;
 if (!tokenForLog) {
-  console.warn('⚠️ [Vercel Build] A variável de ambiente STRAPI_BACKEND_TOKEN não foi encontrada!');
+  console.warn('⚠️ [Vercel Build] A variável de ambiente STRAPI_BACKEND_TOKEN  não foi encontrada!');
 } else {
   console.log('✅ [Vercel Build] A variável STRAPI_BACKEND_TOKEN foi carregada com sucesso.');
 }
