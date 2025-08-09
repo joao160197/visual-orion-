06e2ee0af6250fd8056b7201f5059458caa2f8e1
João Paulo <ramoslopesjoaopaulo@gmail.com>
2025-08-08 21:14:24 -0300

    chore: force new build

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,11 @@ export function getStrapiURL(path = ""): string {
   const nextPublicStrapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
 
   console.log("--- DEBUGGING getStrapiURL ---");
+  console.log(`process.env.NEXT_PUBLIC_STRAPI_URL: ${nextPublicStrapiUrl}`);
+  console.log(`process.env.STRAPI_URL: ${strapiUrl}`);
+  const finalUrl = nextPublicStrapiUrl || strapiUrl || "http://localhost:1337";
+  console.log(`Final base URL selected: ${finalUrl}`);
   console.log("--- END DEBUGGING ---");
 
-  return `${nextPublicStrapiUrl || strapiUrl || "http://localhost:1337"}${path}`;
+  return `${finalUrl}${path}`;
 }
